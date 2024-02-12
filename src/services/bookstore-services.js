42fa941c3344c4996e2f760cb71f5b844578353a
Roma Hurnovich <zebomenn@bk.ru>
2024-02-12 20:06:27 +0300

    Modified commit

--- src/services/bookstore-services.js
+++ src/services/bookstore-services.js
@@ -4,12 +4,12 @@ export default class BookstoreService {
 
    data = [
       {id: 1, 
-      title: "FUCKING LIFE",
+      title: "Production-Ready Microservices",
       author: "Roma Vellas",
       price: 32,
       coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'},
       {id: 2, 
-         title: "FUCKING JOB",
+         title: "Release It!",
          author: "Roma Vellas",
          price: 45,
          coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'}
